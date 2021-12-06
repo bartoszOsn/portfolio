fd2125f9ebfd923d0adceabc48135616575d845e
bartoszOsn <osinskibartosz24@gmail.com>
2021-12-06 20:14:43 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
+
+console.dir(logo);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.App}>
+      <header className={classes.AppHeader}>
+        <img src={logo} className={classes.AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={classes.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
